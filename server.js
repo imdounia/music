6eb723b7216983bc749db7f3a1e97ffd4adce8a0
dounia <douniabakhkhouch2000@gmail.com>
2021-02-09 22:32:02 +0100

    init service

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-//const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 
 server.use(middlewares)
 server.use(router)
-//server.listen(port);
+server.listen(port);
 server.listen(3000, () => {
   console.log('JSON Server is running')
 })
